074afca329990343e9aa29db7407f4c7528b89b0
hadeer <hadeeressam21@gmail.com>
2025-07-01 23:49:54 +0300

    add packages for Chard Display with animation

--- constant/Color.js
+++ constant/Color.js
@@ -2,7 +2,8 @@ const COLORS = {
     'gray': "#968D8D",
     'black': "#202123",
     'pink': "#EC275F",
-    'white': "#FFFFFF"
+    'white': "#FFFFFF",
+    "baby_blue": "#D4E0EE"
 }
 
 export default COLORS
