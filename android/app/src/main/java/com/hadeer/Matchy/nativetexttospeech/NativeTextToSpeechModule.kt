54eb914e468b8c008ae31eb79a545f499f4ab1cb
hadeer <hadeeressam21@gmail.com>
2025-07-01 17:09:34 +0300

    add TextToSpeech feature in Native Module and displayed in JS

--- android/app/src/main/java/com/hadeer/Matchy/nativetexttospeech/NativeTextToSpeechModule.kt
+++ android/app/src/main/java/com/hadeer/Matchy/nativetexttospeech/NativeTextToSpeechModule.kt
@@ -1,8 +1,10 @@
 package com.nativemodules
 
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
+import java.util.Locale
 
 class NativeTextToSpeechModule(reactContext : ReactApplicationContext):
         NativeTextToSpeechSpec(reactContext), TextToSpeech.OnInitListener
@@ -12,20 +14,44 @@ class NativeTextToSpeechModule(reactContext : ReactApplicationContext):
     }
     override fun getName() = NAME
     private var tts : TextToSpeech? = null
+    private var pendingSpeak = false
     override fun speak(promise: Promise?) {
         if(tts == null){
             tts = TextToSpeech(reactApplicationContext ,this)
-            promise?.resolve("Initialize")
+            pendingSpeak = true
+            promise?.resolve("Initializing TTS....")
         }else{
-            tts?.setPitch(1.0f)
-            tts?.speak("Match Matchy", TextToSpeech.QUEUE_FLUSH, null, "tts1")
-            promise?.resolve("Sound Played")
+            playSpeech(promise)
+//            tts?.setPitch(1.0f)
+//            tts?.speak("Match Matchy", TextToSpeech.QUEUE_FLUSH, null, "tts1")
+//            promise?.resolve("Sound Played")
         }
     }
 
-    override fun onInit(p0: Int) {
-        TODO("Not yet implemented")
+    private fun playSpeech(promise : Promise?){
+        tts?.setPitch(1.0f)
+        val result = tts?.speak("Match Matchy", TextToSpeech.QUEUE_FLUSH, null, "tts1")
+        if(result == TextToSpeech.SUCCESS){
+            promise?.resolve("Sound Played")
+        }
+        else{
+            promise?.reject("TTS_ERROR" , "Failed to speak text")
+        }
     }
 
-
+    override fun onInit(status: Int) {
+        if(status == TextToSpeech.SUCCESS){
+            val res = tts?.setLanguage(Locale.US)
+            if(res == TextToSpeech.LANG_MISSING_DATA ||
+                res == TextToSpeech.LANG_NOT_SUPPORTED){
+                Log.e("Native Text To Speech" , "something went wrong $status")
+            }
+            else if(pendingSpeak){
+                playSpeech(null)
+                pendingSpeak = false
+            }
+        }else{
+            Log.e(NAME , "Initialization failed with status : $status")
+        }
+    }
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,29 @@
 import COLORS from '@/constant/Color'
+import NativeTextToSpeech from '@/specs/NativeTextToSpeech'
 import { useRouter } from 'expo-router'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Image, StyleSheet, View } from 'react-native'
 
 
 const OnBoarding = () => {
     const route = useRouter()
 
+    function welcomingText() {
+        NativeTextToSpeech.speak()
+            .then((res) => {
+                console.log("TTS works successfully ", res)
+            })
+            .catch((error) => {
+                console.log("TTS error ", error)
+            })
+    }
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         route.push("/mainHome")
-    //     }, 5000)
-    // })
+    useEffect(() => {
+        welcomingText()
+        setTimeout(() => {
+            route.push("/mainHome")
+        }, 5000)
+    })
 
     return (
         <View style={styles.mainScreen}>
